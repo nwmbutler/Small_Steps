1756651f60bb30b4d4a6f64e75c044062ff0d746
Nick Butler <nwmbutler@gmail.com>
2020-07-24 19:05:09 +0100

    environmental variable added to CalculatorForm

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
         this.state = { apiResponse: "" };
     }
     callAPI() {
-        fetch(`${process.env.REACT_APP_SERVER}`)
+        fetch("")
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
