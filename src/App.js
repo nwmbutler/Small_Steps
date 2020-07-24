2791e84f326d73c6fe0a16155b55b4e4e0008687
Nick Butler <nwmbutler@gmail.com>
2020-07-24 11:36:08 +0100

    profile removed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Carbon Calculator</h1>
-                <p className="App-intro">{this.state.apiResponse}</p>
+                {/* <p className="App-intro">{this.state.apiResponse}</p> */}
                 </header>
             </div>
         );
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App/>);
   const linkElement = getByText(/Carbon Calculator/i);
   expect(linkElement).toBeInTheDocument();
 });
