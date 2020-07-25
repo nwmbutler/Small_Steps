85b02235eee04db93fe9dde57ccc123cb2de2b12
minsi yang <yangminsi@gmail.com>
2020-07-25 17:30:44 +0100

    Added CalculateForm test for origin input box

--- src/components/CalculateForm.test.js
+++ src/components/CalculateForm.test.js
@@ -3,7 +3,14 @@ import { render } from '@testing-library/react';
 import CalculateForm from './CalculateForm';
 
 test('renders Calculate button', () => {
-  const { getByText } = render(<CalculateForm/>);
+  const { getByText } = render(<CalculateForm />);
   const linkElement = getByText('Calculate');
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders origin input field', () => {
+  const { getByPlaceholderText } = render(<CalculateForm />);
+  const text = getByPlaceholderText('Choose starting point...');
+  expect(text).toBeInTheDocument();
+});
+
