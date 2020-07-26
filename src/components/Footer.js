78d38b1839c0aca21b34e356b3d123d567216d26
minsi yang <yangminsi@gmail.com>
2020-07-26 15:59:48 +0100

    Footer name adjustment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
       <footer>
         <div class="copyright py-4 text-center text-black fixed-bottom">
           <div class="container">
-            <small>Copyright © Your Website 2020</small>
+            <small>Copyright © Small Steps 2020</small>
           </div>
         </div>
       </footer>
