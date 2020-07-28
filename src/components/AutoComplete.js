05f243c724a0a68f6940599df83a7f6a56118ffa
Rich Ewin <richardewinlambert@gmail.com>
2020-07-28 15:03:12 +0100

    AutoComplete.js created

---
+++ src/components/AutoComplete.js
@@ -0,0 +1,69 @@
+import React from "react";
+import useOnclickOutside from "react-cool-onclickoutside";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+const Autocomplete = () => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {},
+    debounce: 300,
+  });
+
+  const ref = useOnclickOutside(() => {
+    clearSuggestions();
+  });
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = ({ description }) => () => {
+    setValue(description, false);
+    clearSuggestions();
+
+    getGeocode({ address: description })
+      .then((results) => getLatLng(results[0]))
+      .then(({ lat, lng }) => {
+        console.log("Coordinates:", { lat, lng });
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={id} onClick={handleSelect(suggestion)}>
+        {main_text} {secondary_text}
+        </li>
+      );
+    });
+
+  return (
+    <div ref={ref}>
+      <input
+        value={value}
+        onChange={handleInput}
+        disabled={!ready}
+        placeholder="Origin"
+      />
+      {status === "OK" && <ul>{renderSuggestions()}</ul>}
+    </div>
+  );
+};
+
+export default Autocomplete;
