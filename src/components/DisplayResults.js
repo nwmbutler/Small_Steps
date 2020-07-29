c808e0edd59e3fc9d48662652ea63d390053feba
minsi yang <yangminsi@gmail.com>
2020-07-29 19:02:37 +0100

    Added animation to DisplayResult.js

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -20,7 +20,7 @@ export default class DisplayResults extends React.Component {
     return (
       <div class="display">
         <Container>
-          <Row className="justify-content-center" id="displayResult">
+          <Row className="justify-content-center animate__animated animate__zoomInDown animate__slow" id="displayResult">
             <div>
               <span class="d-block bg-white">
                 Your carbon footprint: {this.props.result}
@@ -28,17 +28,19 @@ export default class DisplayResults extends React.Component {
             </div>
           </Row>
 
-          <Row className="justify-content-center" id="displayResult">
+          <Row className="justify-content-center animate__animated animate__zoomIn animate__delay-2s" id="displayResult">
+            <Link to="/alternatives">
+              <Button variant="success">See Alternatives</Button>
+            </Link>
+          </Row>
+
+          <Row className="justify-content-center animate__animated animate__zoomIn animate__delay-3s" id="displayResult">
             <Link to="/">
               <Button variant="success">New Journey</Button>
             </Link>
           </Row>
 
-          <Row className="justify-content-center" id="displayResult">
-            <Link to="/alternatives">
-              <Button variant="success">See Alternatives</Button>
-            </Link>
-          </Row>
+
         </Container>
       </div>
     );
--- src/components/DisplayResults2.js
+++ src/components/DisplayResults2.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import { Link } from 'react-router-dom';
 export default class DisplayResults2 extends React.Component {
   constructor(props) {
@@ -20,30 +21,29 @@ export default class DisplayResults2 extends React.Component {
     return (
       <div class="display">
         <Container>
-          <Link to="/">
-            <Button variant="success">New Journey</Button>
-          </Link>
-        </Container>
-
-        
-        <br />
-        <br />
-        <div>
+          <Row className="justify-content-center" id="displayResult">
             <span class="d-block bg-white">
               Your carbon old footprint is: {this.props.originalresult}
             </span>
-          </div>
-        <Container>
-          <div>
+          </Row>
+
+          <Row className="justify-content-center" id="displayResult">
             <span class="d-block bg-white">
               Your carbon new footprint is: {this.props.result}
             </span>
-          </div>
-        </Container>
-        <Container>
-          <a href = 'https://www.woodlandtrust.org.uk/support-us/give/donations/'>
-            <Button variant="success">Donate</Button>
-          </a>
+          </Row>
+
+          <Row className="justify-content-center" id="displayResult">
+            <Link to="/">
+              <Button variant="success">New Journey</Button>
+            </Link>
+          </Row>
+
+          <Row className="justify-content-center" id="displayResult">
+            <a href='https://www.woodlandtrust.org.uk/support-us/give/donations/' target="_blank" rel="noopener noreferrer">
+              <Button variant="success">Donate</Button>
+            </a>
+          </Row>
         </Container>
       </div>
     );
